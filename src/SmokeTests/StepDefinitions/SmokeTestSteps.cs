22c9c84a8989b2c941a9e88e714d225ee5254613
James Kovacs <jkovacs@post.harvard.edu>
2016-02-03 10:18:08 -0700

    Initial setup of SpecFlow and Selenium

---
+++ src/SmokeTests/StepDefinitions/SmokeTestSteps.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow;
+
+namespace SmokeTests.StepDefinitions
+{
+    [Binding]
+    public class SmokeTestSteps
+    {
+        private IWebDriver driver;
+
+        [Given(@"I am using (.*)")]
+        public void GivenIAmUsing(string browser)
+        {
+            switch (browser)
+            {
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "PhantomJS":
+                    driver = new PhantomJSDriver();
+                    break;
+                default:
+                    throw new ArgumentException("Unknown browser");
+            }
+        }
+
+        [When(@"I browse to '(.*)'")]
+        public void WhenIBrowseTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [When(@"I search for '(.*)'")]
+        public void WhenISearchFor(string term)
+        {
+            var query = driver.FindElement(By.Name("q"));
+            query.SendKeys(term);
+            query.Submit();
+        }
+
+        [Then(@"the page title should start with '(.*)'")]
+        public void ThenThePageTitleShouldStartWith(string title)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => d.Title.StartsWith(title, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        [AfterScenario]
+        public void Cleanup()
+        {
+            driver?.Quit();
+        }
+    }
+}
---
+++ src/SmokeTests/StepDefinitions/SmokeTestsBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace SmokeTests.StepDefinitions
+{
+    [Binding]
+    public static class SmokeTestsBootstrapper
+    {
+        private static Process process;
+
+        [BeforeTestRun]
+        public static void Startup()
+        {
+            // kill off existing IIS Express instance if present
+
+            // start IIS Express
+//            var startInfo = new ProcessStartInfo("path to iis express", "arguments");
+//            process = Process.Start(startInfo);
+        }
+
+        [AfterTestRun]
+        public static void Cleanup()
+        {
+            // stop IIS Express
+            process?.Kill();
+        }
+    }
+}
