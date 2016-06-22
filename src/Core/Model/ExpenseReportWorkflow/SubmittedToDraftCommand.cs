49dd5f3c5b2c9cc51cc877c937a784eb358b0eab
Josh Williams <josh.williams@praeses.com>
2016-06-22 10:39:47 -0500

    Awesomeness fail

---
+++ src/Core/Model/ExpenseReportWorkflow/SubmittedToDraftCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearMeasure.Bootcamp.Core.Model.ExpenseReportWorkflow
+{
+    public class SubmittedToDraftCommand : StateCommandBase
+    {
+        public override string TransitionVerbPastTense => "Withdrawn";
+        public override ExpenseReportStatus GetBeginStatus()
+        {
+            return ExpenseReportStatus.Submitted;
+        }
+
+        public override string TransitionVerbPresentTense => "Withdraw";
+        protected override ExpenseReportStatus GetEndStatus()
+        {
+            return ExpenseReportStatus.Draft;
+        }
+
+        protected override bool userCanExecute(Employee currentUser, ExpenseReport report)
+        {
+            return currentUser == report.Submitter;
+        }
+    }
+}
---
+++ src/UnitTests/Core/Model/ExpenseReportWorkflow/SubmittedToDraftCommandTester.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClearMeasure.Bootcamp.Core.Features.Workflow;
+using ClearMeasure.Bootcamp.Core.Model;
+using ClearMeasure.Bootcamp.Core.Model.ExpenseReportWorkflow;
+using NUnit.Framework;
+
+namespace ClearMeasure.Bootcamp.UnitTests.Core.Model.ExpenseReportWorkflow
+{
+    public class SubmittedToDraftCommandTester : StateCommandBaseTester
+    {
+        protected override StateCommandBase GetStateCommand(ExpenseReport order, Employee employee)
+        {
+            return new SubmittedToDraftCommand();
+        }
+
+        [Test]
+
+        public void ShouldBeValid()
+        {
+            var report = new ExpenseReport();
+            report.Status = ExpenseReportStatus.Submitted;
+
+            var employee = new Employee();
+            report.Submitter = employee;
+
+            var command = new SubmittedToDraftCommand();
+            Assert.That(command.IsValid(new ExecuteTransitionCommand(report, null, employee, new DateTime())), Is.True);
+
+        }
+
+        [Test]
+        public void ShouldNotBeValidInWrongStatus()
+        {
+            var report = new ExpenseReport();
+            report.Status = ExpenseReportStatus.Draft;
+            var employee = new Employee();
+            report.Approver = employee;
+
+            var command = new SubmittedToDraftCommand();
+            Assert.That(command.IsValid(new ExecuteTransitionCommand(report, null, employee, new DateTime())), Is.False);
+        }
+
+        
+    }
+}
